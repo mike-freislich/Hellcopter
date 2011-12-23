9de0fb90d1c2c0536726f6202ae7873243d20098
Mike Freislich <mike.freislich@gmail.com>
2011-12-23 11:32:53 +0200

    Added Sprite base class

---
+++ Hellcopter/Sprite.h
@@ -0,0 +1,18 @@
+//
+//  Sprite.h
+//  Hellcopter
+//
+//  Created by Mike Freislich on 2011/12/22.
+//  Copyright (c) 2011 Clue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sprite : NSObject
+
+- (id) init;
+- (void) initWithTexture:(NSString*) path;
+
+
+
+@end
---
+++ Hellcopter/Sprite.m
@@ -0,0 +1,14 @@
+//
+//  Sprite.m
+//  Hellcopter
+//
+//  Created by Mike Freislich on 2011/12/22.
+//  Copyright (c) 2011 Clue. All rights reserved.
+//
+
+#import "Sprite.h"
+#import <OpenGL/glu.h>
+
+@implementation Sprite
+
+@end
